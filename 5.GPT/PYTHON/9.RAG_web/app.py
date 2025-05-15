9cc3c6e33e8afb79f56b6613eea45640c9ea8191
pyan <pa98213@naver.com>
2025-05-15 09:41:39 +0900

    RAG 완료

--- 5.GPT/PYTHON/9.RAG_web/app.py
+++ 5.GPT/PYTHON/9.RAG_web/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, send_from_directory
 import os
 
-from database import create_vector_db, answer_question
+from database2 import initialize_vector_db, create_vector_db, answer_question
 
 DATA_DIR = './DATA'
 
@@ -16,6 +16,7 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    initialize_vector_db()
     file = request.files['file']
     if file:
         file_path = os.path.join(DATA_DIR, file.filename)
